d1f19ca30ed84039585cfd738135a1e7878570c0
Durgesh <durgesgkhot274@gmail.com>
2024-04-02 17:23:51 +0530

    added comments in the main class

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("trying to add via intellij");
+        System.out.println("Addition: "+ Calculator.add(2, 1));
     }
 }
+/*
+       master
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("trying to add via intellij");
+    }
+}
+
+        calculator
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("trying to add via intellij");
+        System.out.println("Addition"+ Calculator.add(2, 1));
+    }
+}
+ */
