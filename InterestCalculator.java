0ad216b3f4a59603b86d108a9b122b2caadfaa97
Durgesh <durgesgkhot274@gmail.com>
2024-04-04 14:11:44 +0530

    Added Interest Calculator

---
+++ InterestCalculator.java
@@ -0,0 +1,5 @@
+public class InterestCalculator {
+    public static int calculate(int principal, int rate, int year){
+        return (principal * rate * year)/100;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("trying to add via intellij");
+        System.out.println("Addition: "+ Calculator.add(2, 1));
+        System.out.println("Interest: " + InterestCalculator.calculate(100000, 8, 5));
     }
 }
+/*
+       master
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("trying to add via intellij");
+    }
+<<<<<<< HEAD
+}
+=======
+}
+
+        calculator
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("trying to add via intellij");
+        System.out.println("Addition"+ Calculator.add(2, 1));
+    }
+}
+ */
+
